40914548fa8b4b7a1530b565fa318e6c0bd257be
irinatu17 <iratushina17@gmail.com>
2020-05-05 21:40:50 +0000

    Connect register function with DB users collection

--- mycookbook/routes.py
+++ mycookbook/routes.py
@@ -1,9 +1,10 @@
-from flask import render_template, url_for, flash, redirect
-from mycookbook import app
-from mycookbook  import mongo
+from flask import render_template, url_for, flash, redirect, request, session
+from mycookbook import app, mongo
+from werkzeug.security import generate_password_hash, check_password_hash
 from mycookbook.forms import RegistrationForm, LoginForm
 from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
+
 
 # MongoDB Collections
 users_coll = mongo.db.users
@@ -45,12 +46,26 @@ def login():
 # Register
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if 'username' in session:
+        return redirect(url_for('home'))
+
     form = RegistrationForm()
     if form.validate_on_submit():
         # Variable for users collection
         users = users_coll
-
-
-        flash(f'Account created for {form.username.data}!')
-        return redirect(url_for('home'))
+        registered_user = users_coll.find_one({'username': request.form['username']})
+        if registered_user:
+            flash("Sorry, this username is already taken!")
+            return redirect(url_for('register'))
+        else:
+            hashed_password = generate_password_hash(request.form['password'])
+            new_user = {
+                "username": request.form['username'],
+                "password": hashed_password,
+                "user_recipes": [],
+            }
+            users.insert_one(new_user)
+            session["username"] = request.form['username']
+            flash(f'Your account have been successfully created.')
+            return redirect(url_for('home'))
     return render_template('register.html', form=form,  title='Register')
