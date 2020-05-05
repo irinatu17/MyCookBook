a299a01f5a7609b8b2da4406a90a3ca0f8383f2e
irinatu17 <iratushina17@gmail.com>
2020-05-05 13:00:33 +0000

    Add configuration for connection to MongoDB

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
 import os
 from flask import Flask, render_template, url_for, flash, redirect
+
+if os.path.exists("env.py"):
+   import env
+
 app = Flask(__name__)
 
+app.config['MONGODB_NAME'] = os.environ.get('MONGODB_NAME')
+app.config['MONGODB_URI'] = os.environ.get('MONGODB_URI')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 @app.route("/")
 def test():
