ea91c327488b93fc3c7574be3c71912ccb76a672
irinatu17 <iratushina17@gmail.com>
2020-05-06 16:21:17 +0000

    Basics of add_recipe form added

--- mycookbook/forms.py
+++ mycookbook/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, IntegerField, SelectField
 from wtforms.validators import DataRequired, Length, EqualTo
 
 
@@ -35,9 +35,29 @@ class ChangePasswordForm(FlaskForm):
                                  validators=[DataRequired(),
                                              Length(min=3, max=15)])
     new_password = PasswordField('New Password', validators=[DataRequired(),
-                                                             Length(min=3, 
+                                                             Length(min=3,
                                                                     max=15)])
     confirm_new_password = PasswordField('Confirm New Password',
                                          validators=[DataRequired(),
                                                      Length(min=3, max=15)])
     submit = SubmitField('Change Password')
+
+
+class AddRecipe(FlaskForm):
+    recipe_name = StringField('Recipe Name',
+                              validators=[DataRequired(),
+                                          Length(min=3, max=25)])
+    recipe_description = TextAreaField('Recipe Description',
+                                       validators=[DataRequired(),
+                                                   Length(min=20, max=300)])
+    cooking_time = IntegerField('Cooking Time', validators=[DataRequired()])
+    servings = IntegerField('Number of Servings', validators=[DataRequired()])
+    image = StringField('Recipe Image')
+    ingredients = TextAreaField('Ingredients',
+                                validators=[DataRequired()])
+    directions = TextAreaField('Directions',
+                               validators=[DataRequired()])
+    cuisine_type = SelectField('Cuisine Type')
+    meal_type = SelectField('Meal Type')
+    diet_type = SelectField('Diet Type')
+    submit = SubmitField('Add Recipe')
