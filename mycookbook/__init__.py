4de28b75a5b10ad314287fa700ebb4f6289ea484
irinatu17 <iratushina17@gmail.com>
2020-05-14 12:09:59 +0000

    Remove MONGODB_NAME from .env.py and assign it in __init__.py

--- mycookbook/__init__.py
+++ mycookbook/__init__.py
@@ -7,7 +7,7 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 # Config Settings & Environmental Variables located in env.py
-app.config['MONGODB_NAME'] = os.environ.get('MONGODB_NAME')
+app.config['MONGODB_NAME'] = "MyCookBook"
 app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
