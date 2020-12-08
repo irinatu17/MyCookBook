793972a4f72c19dcd6487f8dcd64f7677be4d416
Irina <ira_tushina@mail.ru>
2020-12-08 21:25:59 +0000

    Flash messages styling updated

--- mycookbook/static/js/mycookbook.js
+++ mycookbook/static/js/mycookbook.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $('.sidenav').sidenav(); 
     $(".dropdown-trigger").dropdown();
     $('select').formSelect();
+
     //CREDITS: The approach of validation select option for a dropdown is taken from
     //StackOverflow: https://stackoverflow.com/questions/34248898/how-to-validate-select-option-for-a-materialize-dropdown
     $("select[required]").css({display: "block", height: 0, padding: 0, width: 0, position: "absolute"});
@@ -16,4 +17,4 @@ $(document).ready(function() {
     $("#btn-home").mouseover(function() {
         $(this).css("transform", "scale(1.05)");
     });
- });
+});
